b8e304b05046c84361e4ca67fe0ddeaae2f56f08
ShawnBorn-Newton <shawn.newton3@gmail.com>
2017-07-28 08:37:09 -0400

    added random movie view model

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,20 +4,32 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+       
 
         // GET: Movies
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
+            
+            var customers = new List<Customer>
+        {
+            new Customer {Name = "Cusromer 1"},
+            new Customer {Name = "Cusromer 2"}
+        };
+            var viewModel = new RandomMovieViewModel();
+            {
+                Movie = movie;
+                Customers = customers;
+            }
 
-            return View(movie);
+            return View(viewModel);
         }
-
         [Route("movies/released/{year}/{month:regex(\\d{4}):range(1, 12)}")]
         public ActionResult ByReleaseDate(int year, int month)
         {
